f99b3fc9c5c42959bd915dda551681c2b20fbb0a
Evabas <51789607+Evabas@users.noreply.github.com>
2020-12-04 09:51:15 +0100

    Add files via upload

---
+++ app/Classe.php
@@ -0,0 +1,8 @@
+<?php
+namespace App;
+// Dans chaque classes PHP du dossier app, il faut ajouter en début de script la ligne ci-dessus
+// Dès que vous voulez utiliser une des classes de ce dossier app, il suffit d’utiliser les lignes de code telle que celle-ci :
+
+//     use App\nomDeLaClasse as nomDeLaClasse;
+
+// Cela appellera automatiquement la classe ayant pour nom nomDeLaClasse, et vous pourrez l’utiliser sans problème.
