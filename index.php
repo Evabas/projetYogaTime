e2682f0b25274b5d720c4c1beb3672130193e668
Evabas <51789607+Evabas@users.noreply.github.com>
2020-12-05 15:47:46 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -5,6 +5,39 @@ use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
 $loader = new FilesystemLoader(__DIR__ . '/templates');
-$twig = new Environment($loader);
+$twig = new Environment($loader, [
+    'cache' => false,
+]);
 
-echo $twig->render('home.twig');
+
+
+if (isset($_GET['p'])) {
+    $page= $_GET['p'];
+}
+
+switch ($page) {
+    case 'home':
+        echo $twig->render('/home.twig');
+    break;
+    case 'lessons':
+        echo $twig->render('/lessons.twig');
+    break;
+    case 'planning':
+        echo $twig->render('/planning.twig');
+    break;
+    case 'team':
+        echo $twig->render('/team.twig');
+    break;
+    case 'price':
+        echo $twig->render('/price.twig');
+    break;
+    case 'contact':
+        echo $twig->render('/contact.twig');
+    break;
+    case 'subscriber':
+        echo $twig->render('/subscriber.twig');
+    break;
+    default:
+        header('HTTP/1.0 404 Not Found');
+        echo $twig->render('/404.twig');
+}
