8d10232e1ffb224f4b5acabdcd295f20414f4ac4
Evabas <51789607+Evabas@users.noreply.github.com>
2020-12-16 09:59:14 +0100

    Add files via upload

--- controller/frontend.php
+++ controller/frontend.php
@@ -1,6 +1,8 @@
 <?php
+use model\MemberManager as MemberManager;
+use model\VideoManager as VideoManager;
 
-require_once('./model/MemberManager.php');
+// registration, connection
 
 function member()
 {
@@ -31,3 +33,55 @@ function pseudoExist()
     return $pseudoExist;
 }
 
+// videos
+
+function listVideos()
+{
+    $videoManager = new VideoManager(); 
+    $videos = $videoManager->getVideos(); 
+ 
+  return $videos;
+
+}
+
+// function video()
+// {
+//     $videoManager = new videoManager();
+//     $video = $videoManager->getvideo($_GET['id']);
+
+
+
+//     // require('view/frontend/videoView.php');
+// }
+
+// function newvideo()
+// {
+//     $addvideoManager = new videoManager();
+
+//     $addvideo = $addvideoManager->addvideo();
+
+//     return $addvideo;
+// }
+
+// function removevideo($videoId)
+// {
+//     $removevideoManager = new videoManager();
+
+//     $removevideo = $removevideoManager->suppressvideo($videoId);
+
+//     return $removevideo;
+
+// }
+
+//boutons
+// function approve($commentId)
+// {
+//     $commentManager = new CommentManager();
+//     $approve = $commentManager->approbationCommentaire($commentId); 
+// }
+
+// function suppress($commentId)
+// {
+//     $commentManager = new CommentManager();
+//     $suppress = $commentManager->suppressionCommentaire($commentId); 
+// }
